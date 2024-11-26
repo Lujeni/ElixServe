5cd9cddb26f8148a854191d6997c426155376a91
Lujeni <julien@thebault.co>
2024-11-26 21:22:42 +0100

    feat: response 200

--- lib/main.ex
+++ lib/main.ex
@@ -1,21 +1,24 @@
 defmodule Server do
   use Application
 
+  @spec start(:normal | :supervised, term()) :: {:ok, pid()} | {:error, term()}
   def start(_type, _args) do
     Supervisor.start_link([{Task, fn -> Server.listen() end}], strategy: :one_for_one)
   end
 
+  @spec listen() :: :ok
   def listen() do
-    IO.puts("Ola")
     {:ok, socket} = :gen_tcp.listen(4221, [:binary, active: false, reuseaddr: true])
-    {:ok, _client} = :gen_tcp.accept(socket)
+    {:ok, client} = :gen_tcp.accept(socket)
+    :gen_tcp.send(client, "HTTP/1.1 200 OK\r\n\r\n")
   end
 end
 
 defmodule CLI do
+  @spec main([String.t()]) :: :ok
   def main(_args) do
     # Start the Server application
-    {:ok, _pid} = Application.ensure_all_started(:elixserve)
+    {:ok, _pid} = Application.ensure_all_started(:codecrafters_http_server)
 
     # Run forever
     Process.sleep(:infinity)
